0d32276bbad7a03652acf3783fd73ae21c33a046
davicaetano <davicaetano@gmail.com>
2024-07-20 03:16:41 -0400

    Implement conditional state

--- app/src/main/java/com/davicaetano/basicstatecodelab/WellnessScreen.kt
+++ app/src/main/java/com/davicaetano/basicstatecodelab/WellnessScreen.kt
@@ -23,11 +23,17 @@ fun WaterCounter(modifier: Modifier = Modifier) {
         modifier = modifier.padding(16.dp)
     ) {
         var count by remember { mutableStateOf(0) }
-        Text(
-            text = "You've had $count glasses.",
-            modifier = modifier.padding(16.dp)
-        )
-        Button(onClick = { count++ }) {
+        if (count > 0) {
+            Text(
+                text = "You've had $count glasses.",
+                modifier = modifier.padding(16.dp)
+            )
+        }
+        Button(
+            onClick = { count++ },
+            Modifier.padding(top = 8.dp),
+            enabled = count < 10
+        ) {
             Text(text = "Add one")
         }
     }
