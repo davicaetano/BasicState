82b59f6617eaa4f2b41425acc46598ae441f673d
davicaetano <davicaetano@gmail.com>
2024-07-20 03:46:09 -0400

    Implement TaskItem

--- app/src/main/java/com/davicaetano/basicstatecodelab/WellnessScreen.kt
+++ app/src/main/java/com/davicaetano/basicstatecodelab/WellnessScreen.kt
@@ -1,6 +1,7 @@
 package com.davicaetano.basicstatecodelab
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -10,6 +11,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -24,17 +26,40 @@ fun WaterCounter(modifier: Modifier = Modifier) {
     ) {
         var count by remember { mutableStateOf(0) }
         if (count > 0) {
+            var showTask by remember { mutableStateOf(true) }
+            if (showTask) {
+                WellnessTaskItem(
+                    onClose = { showTask = false },
+                    taskName = "Have you taken your 15 minute walk today?"
+                )
+            }
             Text(
                 text = "You've had $count glasses.",
                 modifier = modifier.padding(16.dp)
             )
         }
-        Button(
-            onClick = { count++ },
-            Modifier.padding(top = 8.dp),
-            enabled = count < 10
-        ) {
-            Text(text = "Add one")
+        Row(Modifier.padding(16.dp)) {
+            Button(
+                onClick = { count++ },
+                Modifier.padding(8.dp),
+                enabled = count < 10
+            ) {
+                Text(text = "Add one")
+            }
+            Button(
+                onClick = { count = 0 },
+                Modifier.padding(8.dp),
+                enabled = count < 10
+            ) {
+                Text(text = "Clear water count")
+            }
         }
+
     }
+}
+
+@Preview
+@Composable
+fun PreviewWellnessScreen() {
+    WellnessScreen(Modifier)
 }
